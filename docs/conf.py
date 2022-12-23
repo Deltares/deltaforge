e6dc31fa64196e0caabea1c5ca39f0231b30c04f
JoerivanEngelen <joerivanengelen@hotmail.com>
2022-12-23 11:51:52 +0100

    Add letters to icon, to change icon to logo

--- docs/conf.py
+++ docs/conf.py
@@ -115,7 +115,7 @@ html_theme_options = {
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-html_logo = "_static/deltaforge_icon.svg"
+html_logo = "_static/deltaforge-logo.svg"
 
 # -- Options for HTMLHelp output ---------------------------------------------
 
